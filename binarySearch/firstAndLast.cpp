99b5379090a6a9d7ebb66418056ed7af6292421d
divyanshu <divyanshulohar2002@gmail.com>
2022-11-27 19:18:38 +0530

    firstAndLast

--- binarySearch/firstAndLast.cpp
+++ binarySearch/firstAndLast.cpp
@@ -1,5 +1,26 @@
 #include<iostream>
 using namespace std;
+
+int firstOcc(int n, int arr[], int x){
+    int s = 0;
+    int e = n-1;
+    int mid = (s+e)/2;
+    int ans = -1;
+    while(s<=e){
+        if(arr[mid]==x){
+            ans = mid+1;
+            e = mid-1;
+        }
+        else if(arr[mid]>x){
+            e = mid-1;
+        }
+        else{
+            s = mid+1;
+        }
+    }
+    return ans;
+}
+
 int main(){
     int n;
     cin>>n;
@@ -9,7 +30,7 @@ int main(){
     }
     int x;
     cin>>x;
-    
+    cout<<firstOcc(n, arr, x)<<endl;;
 
 
     return 0;
